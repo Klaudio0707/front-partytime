e8d8e408832e4132cd6d1f6717cee4ad69124d49
Cláudio Roberto <claudioroberto0707@outlook.com>
2025-08-29 22:33:47 -0300

    inclusão do arquivo vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  publicDir: 'public',
+  server: {
+    port: 5173,
+    open: true,
+    host: true,
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'), 
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+});
